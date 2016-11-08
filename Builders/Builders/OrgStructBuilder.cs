3b21e546b10c010e654ee68508d2bb43b39f7d3a
Paul Rohorzka <paul@paulroho.com>
2016-11-09 00:09:15 +0100

    Moved OrgBuiler into OrgStructBuilder

--- Builders/Builders/OrgStructBuilder.cs
+++ Builders/Builders/OrgStructBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Builders.Domain;
 
 namespace Builders.Builders
@@ -12,6 +13,34 @@ namespace Builders.Builders
             var builder = new OrgBuilder(_orgStruct);
             builder.HasChild(shortName, builderActions);
             return _orgStruct;
+        }
+
+        public class OrgBuilder
+        {
+            private readonly OrgStruct _orgStruct;
+            private readonly Org _org;
+
+            public OrgBuilder(OrgStruct orgStruct, Org org = null)
+            {
+                _orgStruct = orgStruct;
+                _org = org;
+            }
+
+            public void HasChild(string shortName, params Action<OrgBuilder>[] builderActions)
+            {
+                var childOrg = new Org(shortName, "Long" + shortName, OrgType.Normal, _org);
+                _orgStruct.Add(childOrg);
+                var builder = new OrgBuilder(_orgStruct, childOrg);
+                ExecuteActions(builder, builderActions);
+            }
+
+            private void ExecuteActions(OrgBuilder builder, IEnumerable<Action<OrgBuilder>> builderActions)
+            {
+                foreach (var buildAction in builderActions)
+                {
+                    buildAction(builder);
+                }
+            }
         }
     }
 }
