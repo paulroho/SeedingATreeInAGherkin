57b42ffd1453f0553cc3c0b58af2ce178cf124ec
Paul Rohorzka <paul@paulroho.com>
2016-11-10 23:17:29 +0100

    Fixed SQL-Scripts according to new naming

--- Builders/SqlCode/CreateTables.sql
+++ Builders/SqlCode/CreateTables.sql
@@ -1,13 +1,13 @@
-CREATE TABLE Org
+CREATE TABLE OrgUnit
 (
 	Id uniqueidentifier NOT NULL PRIMARY KEY,
 	ShortName nvarchar(20) NOT NULL,
 	Name nvarchar(100) NOT NULL,
-	OrgType int NOT NULL,
-	Fk_IdParentOrg uniqueidentifier NULL
+	OrgUnitType int NOT NULL,
+	Fk_IdParentOrgUnit uniqueidentifier NULL
 )
 
-ALTER TABLE Org
-ADD FOREIGN KEY (Fk_IdParentOrg)
-REFERENCES Org (Id)
+ALTER TABLE OrgUnit
+ADD FOREIGN KEY (Fk_IdParentOrgUnit)
+REFERENCES OrgUnit (Id)
 
--- Builders/SqlCode/InsertRows.sql
+++ Builders/SqlCode/InsertRows.sql
@@ -1,19 +1,19 @@
-INSERT INTO dbo.Org (Id, ShortName, Name, OrgType, Fk_IdParentOrg)
+INSERT INTO dbo.OrgUnit (Id, ShortName, Name, OrgUnitType, Fk_IdParentOrgUnit)
    VALUES ('2EA2966F-64D1-4CA9-A87B-790F697E18B4', 'Board', 'Executive Board', 1, NULL)
-INSERT INTO dbo.Org (Id, ShortName, Name, OrgType, Fk_IdParentOrg)
+INSERT INTO dbo.OrgUnit (Id, ShortName, Name, OrgUnitType, Fk_IdParentOrgUnit)
    VALUES ('2B70C1E0-54D8-4DFB-8C29-7050E80D63E9', 'HOFin', 'Head Office Finance', 2, '2EA2966F-64D1-4CA9-A87B-790F697E18B4')
-INSERT INTO dbo.Org (Id, ShortName, Name, OrgType, Fk_IdParentOrg)
+INSERT INTO dbo.OrgUnit (Id, ShortName, Name, OrgUnitType, Fk_IdParentOrgUnit)
    VALUES ('9B2EB209-6486-4B8A-91B8-6A1706F91012', 'HOTech', 'Head Office Technology', 2, '2EA2966F-64D1-4CA9-A87B-790F697E18B4')
-INSERT INTO dbo.Org (Id, ShortName, Name, OrgType, Fk_IdParentOrg)
+INSERT INTO dbo.OrgUnit (Id, ShortName, Name, OrgUnitType, Fk_IdParentOrgUnit)
    VALUES ('DF142721-993D-44EC-AF42-8995D70128A8', 'ITInfra', 'IT Infrastructure', 3, '9B2EB209-6486-4B8A-91B8-6A1706F91012')
-INSERT INTO dbo.Org (Id, ShortName, Name, OrgType, Fk_IdParentOrg)
+INSERT INTO dbo.OrgUnit (Id, ShortName, Name, OrgUnitType, Fk_IdParentOrgUnit)
    VALUES ('12C970B9-C1AB-44FE-960A-1492FF104F0B', 'SWDevSvc', 'Software Development Services', 3, '9B2EB209-6486-4B8A-91B8-6A1706F91012')
-INSERT INTO dbo.Org (Id, ShortName, Name, OrgType, Fk_IdParentOrg)
+INSERT INTO dbo.OrgUnit (Id, ShortName, Name, OrgUnitType, Fk_IdParentOrgUnit)
    VALUES ('DA1610C9-C8F7-4B23-AB77-FFE4D404DE04', 'SWPmo', 'Software Development PMO', 3, '12C970B9-C1AB-44FE-960A-1492FF104F0B')
-INSERT INTO dbo.Org (Id, ShortName, Name, OrgType, Fk_IdParentOrg)
+INSERT INTO dbo.OrgUnit (Id, ShortName, Name, OrgUnitType, Fk_IdParentOrgUnit)
    VALUES ('BBD441A3-EE6F-4F87-BA34-8D102E03555A', 'SWEng', 'Software Development Engineering', 3, '12C970B9-C1AB-44FE-960A-1492FF104F0B')
 
 
 --SELECT newid()
 --DELETE FROM Org
-SELECT * FROM Org ORDER BY FK_IdParentOrg
+SELECT * FROM OrgUnit ORDER BY Fk_IdParentOrgUnit
