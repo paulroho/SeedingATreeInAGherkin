871a4ecd11950a2554ea428a29e96c71ab2f125f
Paul Rohorzka <paul@paulroho.com>
2016-11-11 05:14:31 +0100

    Added examples in Gherkin for the screenshots

---
+++ Persy/Permission Chain Scenarios.feature
@@ -0,0 +1,10 @@
+﻿Feature: A permission chain can be expressed tersely
+
+Scenario: Permission chain, all objects active
+	Given there is a permission chain 'Ben (Domain\MyUser) --- Grp --- Pos - ad.grp.target'.
+
+	Given there is a permission chain 'Ben (Domain\MyUser) --- Grp (INACTIVE) --- Pos - ad.grp.target'.
+	
+	Given there is a permission chain 'Ben (Domain\MyUser) xxx Grp            --- Pos - ad.grp.target'.
+	
+	Given there is a permission chain 'Ben (Domain\MyUser) xxx Grp (INACTIVE) --- Pos - ad.grp.target'.
---
+++ Persy/PermissionChainBindings.cs
@@ -0,0 +1,33 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Persy
+{
+    [Binding]
+    public class PermissionChainBindings
+    {
+        [Given(@"there is a permission chain '(\w+) \((\w+\\\w+)\) --- (\w+) --- ((?!inaktiv)\w+) - ([\w\.]+)'\.?")]
+        public void AngenommenEsExistiertDieBerechtigungskette_AllesAktiv(string nachname, string fullLogin, string gruppenName, string positionsName, string adGroupName)
+        { }
+
+        [Given(@"there is a permission chain '(\w+) \((\w+\\\w+)\) --- (\w+) \(INACTIVE\) --- ((?!inaktiv)\w+) - ([\w\.]+)'\.?")]
+        public void AngenommenEsExistiertDieBerechtigungskette_G_INAKTIV_NEU(string nachname, string fullLogin, string gruppenName, string positionsName, string adGroupName)
+        { }
+
+        [Given(@"there is a permission chain '(\w+) \((\w+\\\w+)\) xxx (\w+) *--- ((?!inaktiv)\w+) - ([\w\.]+)'\.?")]
+        public void AngenommenEsExistiertDieBerechtigungskette_B2G_INAKTIV_NEU(string nachname, string fullLogin, string gruppenName, string positionsName, string adGroupName)
+        { }
+        // ReSharper disable UnusedParameter.Global
+
+        [Given(@"there is a permission chain '(\w+) \((\w+\\\w+)\) " +
+               @"xxx (\w+) \(INACTIVE\) " +
+               @"--- ((?!inaktiv)\w+) - ([\w\.]+)'\.?")]
+        public void GivenThereIsAPermissionChain_U2G_G_INACTIVE(string lastname, string fullLogin, 
+                                                                string groupName, 
+                                                                string positionName, string adGroupName)
+        {
+            // ...
+        }
+        // ReSharper restore UnusedParameter.Global
+
+    }
+}
